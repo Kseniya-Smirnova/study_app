060ff496ecf9c194970c63e878ab5d5c3bdf956c
Kseniya <Kseniya_Smirnova@epam.com>
2017-03-12 20:30:25 +0300

    The second task

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,8 +7,39 @@ import { Component, ViewEncapsulation } from '@angular/core';
 	providers: [],
 	encapsulation: ViewEncapsulation.None
 })
-export class BreadcrumbsComponent {
+export class SearchComponent {
+
 	constructor() {
 
+	}
+
+	ngOnInit() {
+		console.log('search component: onInit');
+	}
+	ngOnChanges() {
+		console.log('search component: onChanges');
+	}
+	ngDoCheck() {
+		console.log('search component: doCheck');
+	}
+	ngAfterContentInit() {
+		console.log('search component: afterContentInit');
+	};
+	ngAfterContentClicked() {
+		console.log('search component: afterContentClicked');
+	};
+	ngAfterViewInit() {
+		console.log('search component: afterViewInit');
+	};
+	ngAfterViewChecked() {
+		console.log('search component: afterViewChecked');
+	};
+	ngOnDestroy() {
+		console.log('search component: onDestroy');
+	};
+
+
+	findCourse(element: HTMLInputElement):void {
+		console.log(element.value);
 	}
 }
--- src/app/pages/courses/courses.module.ts
+++ src/app/pages/courses/courses.module.ts
@@ -8,11 +8,13 @@ import { routes } from './courses.routes';
 // custom components
 import { CoursesComponent } from './courses.component';
 import { CourseItem } from '../../components/course-item/course-item.component';
+import { SearchComponent } from '../../components/search/search.component';
 
 @NgModule({
 	declarations: [
 		CoursesComponent,
-		CourseItem
+		CourseItem,
+		SearchComponent
 	],
 	imports: [
 		[routes, BrowserModule]
